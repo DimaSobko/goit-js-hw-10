4c4a5033e0c4d19a9b82027b04fa8d5be5a5c280
DimaSobko <sobko.dima@gmail.com>
2023-02-24 13:59:49 +0200

    bug

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import  lodash  from 'lodash.debounce';
+// import  fetchCountries  from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const URL = 'https://restcountries.com/v3.1/name';
+console.log(URL);
