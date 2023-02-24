fe446f80b96121daae84ff9d7a005122e827218a
DimaSobko <sobko.dima@gmail.com>
2023-02-24 14:17:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import  lodash  from 'lodash.debounce';
-// import  fetchCountries  from './fetchCountries';
+import  fetchCountries  from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-const URL = 'https://restcountries.com/v3.1/name';
-console.log(URL);
